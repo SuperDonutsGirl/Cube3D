d1cab357c8760b79a61512b48b6f086612804fa1
Dorian Duraku <dduraku@student.s19.be>
2022-12-15 21:00:38 +0100

    Draw 2D Map

--- srcs/main.c
+++ srcs/main.c
@@ -38,34 +38,22 @@ void	my_mlx_pixel_put(t_cube *cube, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void	draw_player(t_cube *cube, int color, int x_size, int y_size)
+void	draw_walls(t_cube *cube, int color, int x_size, int y_size, int pos_x, int pos_y)
 {
 	for (int i = 0; i < y_size; i++)
 	{
 		for (int j = 0; j < x_size; j++)
 		{
-			my_mlx_pixel_put(cube, i, j, color);
-			printf("je dessine\n");
+			mlx_pixel_put(cube->mlx, cube->window, i + pos_x, j + pos_y, color);
 		}
 	}
 }
 
 int mapX = 8, mapY = 8, mapS = 64;
 
-int map[] =
-		{
-		1, 1, 1, 1, 1, 1, 1, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 1, 1, 1, 1, 1, 1, 1
-};
-
-void	draw_map_2D(t_cube *cube, int color_floor, int color_wall)
+void	draw_map_2D(t_cube *cube, int color_floor, int color_wall, int *map)
 {
+	int pos_x = 0, pos_y = 0;
 	int x, y; //, xo, yo;
 	for (y = 0; y < mapY; y++)
 	{
@@ -73,20 +61,35 @@ void	draw_map_2D(t_cube *cube, int color_floor, int color_wall)
 		{
 			if (map[y * mapX + x] == 1)
 			{
-				draw_player(cube, color_wall, 10, 10);
+				draw_walls(cube, color_wall, 64, 64, pos_x, pos_y);
+				printf("WALL\n");
 			}
 			else
 			{
-				draw_player(cube, color_floor, 10, 10);
-				printf("SOL\n");
+				draw_walls(cube, color_floor, 64, 64, pos_x, pos_y);
+				printf("FLOOR\n");
 			}
+			pos_x += 64;
 		}
+		pos_y += 64;
+		pos_x = 0;
 	}
 }
 
 int	main(void)
 {
 	t_cube	*cube;
+	int map[] =
+			{
+					1, 1, 1, 1, 1, 1, 1, 1,
+					1, 0, 0, 0, 0, 0, 0, 1,
+					1, 0, 0, 0, 0, 0, 0, 1,
+					1, 0, 0, 0, 0, 0, 0, 1,
+					1, 0, 0, 0, 0, 0, 0, 1,
+					1, 0, 0, 0, 0, 0, 0, 1,
+					1, 0, 0, 0, 0, 0, 0, 1,
+					1, 1, 1, 1, 1, 1, 1, 1
+			};
 
 	cube = malloc(sizeof(t_cube));
 	if (!cube)
@@ -98,9 +101,9 @@ int	main(void)
 	cube->img = mlx_new_image(cube->mlx, 1024, 512);
 	cube->address = mlx_get_data_addr(cube->img, &cube->bits_per_pixel,
 									  	&cube->line_length, &cube->endian);
-	draw_map_2D(cube, 0x00FF00, 0xFFFFFF);
-	mlx_put_image_to_window(cube->mlx, cube->window,
-								cube->img, cube->pos_x, cube->pos_y);
+//	mlx_put_image_to_window(cube->mlx, cube->window,
+//								cube->img, cube->pos_x, cube->pos_y);
+	draw_map_2D(cube, 0x00FF00, 0x00FFFF, map);
 	mlx_hook(cube->window, 3, 1L<<0, move_player, cube);
 	mlx_hook(cube->window, 2, 1L<<0, keybinds, cube);
 	mlx_hook(cube->window, 17, 0L, close_on_click, cube);
