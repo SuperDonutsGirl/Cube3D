30eaf4e3b7d446a5c19cbe0332b60bce60abca23
Dorian Duraku <dduraku@student.s19.be>
2022-12-15 21:09:41 +0100

    Draw 2D map opti

--- srcs/main.c
+++ srcs/main.c
@@ -44,15 +44,15 @@ void	draw_walls(t_cube *cube, int color, int x_size, int y_size, int pos_x, int 
 	{
 		for (int j = 0; j < x_size; j++)
 		{
-			mlx_pixel_put(cube->mlx, cube->window, i + pos_x, j + pos_y, color);
+			my_mlx_pixel_put(cube, i + pos_x, j + pos_y, color);
+//			mlx_pixel_put(cube->mlx, cube->window, i + pos_x, j + pos_y, color);
 		}
 	}
 }
 
-int mapX = 8, mapY = 8, mapS = 64;
-
 void	draw_map_2D(t_cube *cube, int color_floor, int color_wall, int *map)
 {
+	int mapX = 8, mapY = 8, mapS = 64;
 	int pos_x = 0, pos_y = 0;
 	int x, y; //, xo, yo;
 	for (y = 0; y < mapY; y++)
@@ -61,12 +61,12 @@ void	draw_map_2D(t_cube *cube, int color_floor, int color_wall, int *map)
 		{
 			if (map[y * mapX + x] == 1)
 			{
-				draw_walls(cube, color_wall, 64, 64, pos_x, pos_y);
+				draw_walls(cube, color_wall, mapS, mapS, pos_x, pos_y);
 				printf("WALL\n");
 			}
 			else
 			{
-				draw_walls(cube, color_floor, 64, 64, pos_x, pos_y);
+				draw_walls(cube, color_floor, mapS, mapS, pos_x, pos_y);
 				printf("FLOOR\n");
 			}
 			pos_x += 64;
@@ -94,16 +94,15 @@ int	main(void)
 	cube = malloc(sizeof(t_cube));
 	if (!cube)
 		return 1;
-	cube->pos_x = 10;
-	cube->pos_y = 25;
+	cube->pos_x = 50;
+	cube->pos_y = 50;
 	cube->mlx = mlx_init();
 	cube->window = mlx_new_window(cube->mlx, 1024, 512, "cub3d");
 	cube->img = mlx_new_image(cube->mlx, 1024, 512);
 	cube->address = mlx_get_data_addr(cube->img, &cube->bits_per_pixel,
 									  	&cube->line_length, &cube->endian);
-//	mlx_put_image_to_window(cube->mlx, cube->window,
-//								cube->img, cube->pos_x, cube->pos_y);
 	draw_map_2D(cube, 0x00FF00, 0x00FFFF, map);
+	mlx_put_image_to_window(cube->mlx, cube->window, cube->img, 0, 0);
 	mlx_hook(cube->window, 3, 1L<<0, move_player, cube);
 	mlx_hook(cube->window, 2, 1L<<0, keybinds, cube);
 	mlx_hook(cube->window, 17, 0L, close_on_click, cube);
