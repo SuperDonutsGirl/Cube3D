7d4f169cca08f20d79a23b5627ff6c257b98ba5c
Dorian Duraku <dduraku@student.s19.be>
2022-12-15 21:45:21 +0100

    player can move accross the map

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,14 @@
 
 #include "../includes/cub3d.h"
 
+void	draw_player(t_cube *cube, int color, int x_size, int y_size)
+{
+	for (int i = 0; i < y_size; i++) {
+		for (int j = 0; j < x_size; j++)
+			mlx_pixel_put(cube->mlx, cube->window, j + cube->pos_x, i + cube->pos_y, color);
+	}
+}
+
 int	move_player(int keycode, t_cube *cube)
 {
 	mlx_clear_window(cube->mlx, cube->window);
@@ -25,8 +33,8 @@ int	move_player(int keycode, t_cube *cube)
 		cube->pos_y -= 5;
 	else
 		printf("Keycode : %d\n", keycode);
-	mlx_put_image_to_window(cube->mlx, cube->window,
-								cube->img, cube->pos_x, cube->pos_y);
+	mlx_put_image_to_window(cube->mlx, cube->window, cube->img, 0, 0);
+	draw_player(cube, 0xFF0053, 10, 10);
 	return 0;
 }
 
@@ -43,10 +51,7 @@ void	draw_walls(t_cube *cube, int color, int x_size, int y_size, int pos_x, int 
 	for (int i = 0; i < y_size; i++)
 	{
 		for (int j = 0; j < x_size; j++)
-		{
 			my_mlx_pixel_put(cube, i + pos_x, j + pos_y, color);
-//			mlx_pixel_put(cube->mlx, cube->window, i + pos_x, j + pos_y, color);
-		}
 	}
 }
 
@@ -54,25 +59,20 @@ void	draw_map_2D(t_cube *cube, int color_floor, int color_wall, int *map)
 {
 	int mapX = 8, mapY = 8, mapS = 64;
 	int pos_x = 0, pos_y = 0;
-	int x, y; //, xo, yo;
+	int x, y;
 	for (y = 0; y < mapY; y++)
 	{
 		for (x = 0; x < mapX; x++)
 		{
 			if (map[y * mapX + x] == 1)
-			{
 				draw_walls(cube, color_wall, mapS, mapS, pos_x, pos_y);
-				printf("WALL\n");
-			}
 			else
-			{
 				draw_walls(cube, color_floor, mapS, mapS, pos_x, pos_y);
-				printf("FLOOR\n");
-			}
 			pos_x += 64;
 		}
 		pos_y += 64;
 		pos_x = 0;
+		mlx_put_image_to_window(cube->mlx, cube->window, cube->img, 0, 0);
 	}
 }
 
@@ -102,7 +102,7 @@ int	main(void)
 	cube->address = mlx_get_data_addr(cube->img, &cube->bits_per_pixel,
 									  	&cube->line_length, &cube->endian);
 	draw_map_2D(cube, 0x00FF00, 0x00FFFF, map);
-	mlx_put_image_to_window(cube->mlx, cube->window, cube->img, 0, 0);
+	draw_player(cube, 0xFF0053, 10, 10);
 	mlx_hook(cube->window, 3, 1L<<0, move_player, cube);
 	mlx_hook(cube->window, 2, 1L<<0, keybinds, cube);
 	mlx_hook(cube->window, 17, 0L, close_on_click, cube);
