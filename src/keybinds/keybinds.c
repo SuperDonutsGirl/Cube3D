28d3e6aa00fa60915c2e0b0ec8c1008ea6b4285a
Pauline <pjl.martineau@gmail.com>
2022-12-29 16:37:18 +0100

    norm for ./src/keybinds

--- src/keybinds/keybinds.c
+++ src/keybinds/keybinds.c
@@ -12,10 +12,23 @@
 
 #include "../../includes/cub.h"
 
-int	move_player(int keycode, t_struct *data)
+static void	key_move_player(int keycode, t_struct *data)
 {
-	mlx_clear_window(data->cube->mlx, data->cube->window);
-	if (keycode == KEY_A) // 0 Ecole, 113 MBP M1 (A GAUCHE)
+	if (keycode == KEY_S)
+	{
+		data->cube->player.px -= data->cube->player.pdx;
+		data->cube->player.py -= data->cube->player.pdy;
+	}
+	else if (keycode == KEY_W)
+	{
+		data->cube->player.px += data->cube->player.pdx;
+		data->cube->player.py += data->cube->player.pdy;
+	}
+}
+
+static void	key_move_cam(int keycode, t_struct *data)
+{
+	if (keycode == KEY_A)
 	{
 		data->cube->player.pa -= 0.1;
 		if (data->cube->player.pa < 0)
@@ -23,12 +36,7 @@ int	move_player(int keycode, t_struct *data)
 		data->cube->player.pdx = cos(data->cube->player.pa) * 5;
 		data->cube->player.pdy = sin(data->cube->player.pa) * 5;
 	}
-	else if (keycode == KEY_S) // 1 Ecole, MBP M1 115 (DERRIERE)
-	{
-		data->cube->player.px -= data->cube->player.pdx;
-		data->cube->player.py -= data->cube->player.pdy;
-	}
-	else if (keycode == KEY_D) //2 Ecole, MBP M1
+	else if (keycode == KEY_D)
 	{
 		data->cube->player.pa += 0.1;
 		if (data->cube->player.pa > 2 * PI)
@@ -36,20 +44,25 @@ int	move_player(int keycode, t_struct *data)
 		data->cube->player.pdx = cos(data->cube->player.pa) * 5;
 		data->cube->player.pdy = sin(data->cube->player.pa) * 5;
 	}
-	else if (keycode == KEY_W) //13 ECOLE, 122 MBP M1 (TOUT DROIT)
-	{
-		data->cube->player.px += data->cube->player.pdx;
-		data->cube->player.py += data->cube->player.pdy;
-	}
-	else if (keycode == ESC) //53 ECOLE, 65307 MBP M1
-		exit(0);
-	mlx_put_image_to_window(data->cube->mlx, data->cube->window, data->cube->img, 0, 0);
-	draw_player(data->cube, 0xFF0053, 10);
-//	draw_rays(data);
-	return 0;
 }
 
-int keypress(int keycode, t_struct *data)
+int	move_player(int keycode, t_struct *data)
+{
+	mlx_clear_window(data->cube->mlx, data->cube->window);
+	if (keycode == KEY_A || keycode == KEY_D)
+		key_move_cam(keycode, data);
+	else if (keycode == KEY_S || keycode == KEY_W)
+		key_move_player(keycode, data);
+	else if (keycode == ESC)
+		exit(0); //Leaks
+	mlx_put_image_to_window(data->cube->mlx, data->cube->window,
+		data->cube->img, 0, 0);
+	draw_player(data->cube, 0xFF0053, 10);
+//	draw_rays(data);
+	return (0);
+}
+
+int	keypress(int keycode, t_struct *data)
 {
 	move_player(keycode, data);
 	return (0);
